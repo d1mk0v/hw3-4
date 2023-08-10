73aa4b73821d85b12a82313898708aac674b8ee5
d1mk0v <79874763588@ya.ru>
2023-08-10 21:22:49 +0500

    hw4.2

---
+++ scripts421.sql
@@ -0,0 +1,7 @@
+alter table student add constraint age_constraint check (age > 16);
+
+alter table student add constraint name_constraint unique (name);
+
+alter table student alter column name set not null;
+
+alter table student alter column age set default 20;
