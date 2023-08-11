e594221b2d0df8d3e1084cc7a978764318caac2a
d1mk0v <79874763588@ya.ru>
2023-08-11 18:15:20 +0500

    hw4.2 - устранение замечаний

--- scripts421.sql
+++ scripts421.sql
@@ -4,4 +4,6 @@ alter table student add constraint name_constraint unique (name);
 
 alter table student alter column name set not null;
 
+alter table faculty add constraint name_color_constraint unique (name, color);
+
 alter table student alter column age set default 20;
