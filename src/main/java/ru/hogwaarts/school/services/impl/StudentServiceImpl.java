629b4db5804d31f77e2d4173ed1cd08de1e19913
d1mk0v <79874763588@ya.ru>
2023-08-12 12:56:03 +0500

    step 1

--- src/main/java/ru/hogwaarts/school/services/impl/StudentServiceImpl.java
+++ src/main/java/ru/hogwaarts/school/services/impl/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwaarts.school.services.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwaarts.school.models.Faculty;
 import ru.hogwaarts.school.models.Student;
@@ -15,42 +17,52 @@ public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
 
+    Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
+
     public StudentServiceImpl(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     @Override
     public Student addStudent(Student student) {
+        logger.info("Was invoked method for create student");
+//        logger.debug("");
         return studentRepository.save(student);
     }
 
     @Override
     public Optional<Student> findStudent(Long id) {
+        logger.info("Was invoked method for find student");
         return studentRepository.findById(id);
     }
 
     @Override
     public Student editStudent(Student student) {
+        logger.info("Was invoked method for edit student");
         return studentRepository.save(student);
     }
 
     @Override
     public void deleteStudent(long id) {
+        logger.info("Was invoked method for delete student");
         studentRepository.deleteById(id);
     }
 
     @Override
     public List<Student> ageFilter(int age) {
+        logger.info("Was invoked method for filter students by age");
         return studentRepository.findByAge(age);
     }
 
     @Override
     public Collection<Student> getStudentsByAgeBetween(int min, int max) {
+        logger.info("Was invoked method for filter students by age between");
         return studentRepository.findByAgeBetween(min, max);
     }
 
     @Override
     public Faculty getStudentFaculty(Long id) {
+        logger.info("Was invoked method for filter students by age between");
         return studentRepository.findById(id).map(Student::getFaculty).orElse(null);
     }
 
